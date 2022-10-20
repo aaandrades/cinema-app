ab81deaa894e224c58da9afe9a02408d4a624585
aaandrades <aaandrades@outlook.com>
2022-10-20 11:31:58 -0500

    :sparkles: Refactor movie page

--- src/app/components/modal/ModalProvider/modal.service.ts
+++ src/app/components/modal/ModalProvider/modal.service.ts
@@ -2,22 +2,22 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ModalService {
-  public showLoader$ = new BehaviorSubject(false);
+  public showLoader$ = new BehaviorSubject({ show: false, message: '' });
 
-  constructor() { }
-  
-  showLoader() {
-    this.showLoader$.next(true);
+  constructor() {}
+
+  showLoader(message = '') {
+    this.showLoader$.next({ show: true, message });
   }
 
   hideLoader() {
-    this.showLoader$.next(false);
+    this.showLoader$.next({ show: false, message: '' });
   }
 
-  subscribeLoader(): Observable<boolean> {
+  subscribeLoader(): Observable<{ show: boolean; message?: string }> {
     return this.showLoader$.asObservable();
   }
 }
